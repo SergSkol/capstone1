e617ec1c8dbf11cb417179923a0b97f93ecafd94
Serg Skol <sps676@gmail.com>
2022-10-24 19:09:42 +0200

    Add about page

--- script/main.js
+++ script/main.js
@@ -26,6 +26,45 @@ const program = [
   },
 ];
 
+const speakers = [
+  {
+    name: 'Anna Shkolnik',
+    photo: './image/speakers/Anna.jpg',
+    title: 'Linz Mittelschule 11, Student',
+    description: 'Became the leading figure in own environment by challenging the boundaries of artistic style with her paintings, which combined abstraction and representation.',
+  },
+  {
+    name: 'Dima Eshkol',
+    photo: './image/speakers/Dima.jpg',
+    title: 'Kyiv Euroland, Student',
+    description: 'His art is incorporating a variety of styles and emphasized on conveying strong emotional or expressive content through abstraction.',
+  },
+  {
+    name: 'Levi Schulmann',
+    photo: './image/speakers/Lev.jpg',
+    title: 'Linz Volksschule 20, Student',
+    description: 'He is working in a variety of media including sculpture and paper cut-outs but is most famous for the masterpieces he created as a painter.',
+  },
+  {
+    name: 'Irene Weisero',
+    photo: './image/speakers/Ira.jpg',
+    title: 'TAK Japan, Team Lead',
+    description: 'Focusing on a collaborative approach in a networked environment, she developed the concept of co-production based on sharing.',
+  },
+  {
+    name: 'Serg Skol',
+    photo: './image/speakers/Serg.jpg',
+    title: 'NVS Ukraine, Commercial Excellence Head',
+    description: 'After working in various fields such as Sales Force manager and Commercial Excellence Head, he is currently in charge of web-design and programming.',
+  },
+  {
+    name: 'Jackie (Benya) Russel',
+    photo: './image/speakers/Jack.jpg',
+    title: 'Founder, Foxtrott',
+    description: 'Russel is primarily a representative of the working environment. High energy and drive make the author ideal for various arts such as flyball or agility. He has a huge amount of energy for his age. It may seem that he never gets tired and will still be energetic after their client is done with it. However, he will not tolerate prejudice, even if it is unintentional.',
+  },
+];
+
 function addElement(elementType, parent, className) {
   const element = document.createElement(elementType);
   element.classList.add(className);
@@ -33,9 +72,8 @@ function addElement(elementType, parent, className) {
   return element;
 }
 
-const programList = document.querySelector('.program-list');
-
 window.onload = () => {
+  const programList = document.querySelector('.program-list');
   program.forEach((item) => {
     const programItem = addElement('div', programList, 'program-item');
     const featuredImage = addElement('img', programItem, 'program-image');
@@ -45,4 +83,40 @@ window.onload = () => {
     const programDescription = addElement('div', programItem, 'program-description');
     programDescription.innerHTML = item.description;
   });
+
+  const speakersList = document.querySelector('.speakers-list');
+
+  speakers.forEach((speaker) => {
+    const speakerItem = addElement('div', speakersList, 'speaker-item');
+    const speakerImage = addElement('img', speakerItem, 'speaker-image');
+    speakerImage.src = speaker.photo;
+    const speakerDetails = addElement('div', speakerItem, 'speaker-details');
+    const speakerName = addElement('div', speakerDetails, 'speaker-name');
+    speakerName.innerHTML = speaker.name;
+    const speakerTitle = addElement('div', speakerDetails, 'speaker-title');
+    speakerTitle.innerHTML = speaker.title;
+    const speakerDescription = addElement('div', speakerDetails, 'speaker-description');
+    speakerDescription.innerHTML = speaker.description;
+  });
 };
+
+// Popup menu
+
+const menuIcon = document.querySelector('.burger-image');
+const hideIcon = document.querySelector('.close-image');
+const menuBox = document.querySelector('.mobile-menu-box');
+const menuItems = document.querySelectorAll('.mobile-menu-item');
+
+menuIcon.addEventListener('click', () => {
+  menuBox.classList.toggle('open-menu');
+});
+
+hideIcon.addEventListener('click', () => {
+  menuBox.classList.toggle('open-menu');
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    menuBox.classList.toggle('open-menu');
+  });
+});
